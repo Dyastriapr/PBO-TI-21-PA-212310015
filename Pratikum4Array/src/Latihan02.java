c337bff348feb1b4f4d44a1f04671c207e144bae
Dyastriapr <114242244+Dyastriapr@users.noreply.github.com>
2022-11-08 23:11:54 +0700

    Pratikum 4 Array

---
+++ Pratikum4Array/src/Latihan02.java
@@ -0,0 +1,23 @@
+public class Latihan02{
+
+	
+	public static void main(String[] args) {
+
+		int[] ages = new int[5];
+
+		//INSERT Value by index
+
+		ages[0] = 1;
+		ages[1] = 2;
+		ages[2] = 3;
+		ages[3] = 4;
+		ages[4] = 5;
+
+		//OUTPUT
+		System.out.println("=Use Loop=");
+		for (int i = 0; i < 5; i++) {
+		System.out.println("ages["+i+"]="+ages[i]);
+		
+			}
+		}
+	}
